943bd363cbec209bc64f88c1e534b077c39d8de1
Your Name <you@example.com>
2025-07-16 19:45:11 +0200

    Signals de bot

--- bot/src/cmds/CmInvite.cpp
+++ bot/src/cmds/CmInvite.cpp
@@ -1,15 +1,8 @@
 #include <Bot.hpp>
 
 // INVITE <nickname> <channel>
-<<<<<<< HEAD
-void Bot::CmInvite(t_msg &msg, int fd) {
-	std::cout << "Command INVITE received, but not implemented yet." << std::endl;
-	std::string response = "JOIN " + msg.trailing + "\r\n";
-	send(fd, response.c_str(), response.size(), MSG_EOR);
-=======
 void Bot::CmInvite(t_msg &msg) {
 	std::string response = "JOIN " + msg.trailing + "\r\n";
 	if (send(_fd, response.c_str(), response.size(), MSG_EOR) < 0)
 		return;
->>>>>>> origin/bot
 }
--- bot/src/cmds/CmPrivmsg.cpp
+++ bot/src/cmds/CmPrivmsg.cpp
@@ -2,22 +2,6 @@
 
 void Bot::CmPrivMsg(t_msg &msg)
 {
-<<<<<<< HEAD
-    std::vector<std::string> words;
-    splitCmd(msg.trailing, words, ' ');
-    std::cout << "Command PRIVMSG received, but not implemented yet." << std::endl;
-
-    if (msg.params[0] == "Bot") {
-        std::cout << "This is a private message." << std::endl;
-        std::string response = "PRIVMSG " + msg.from + " :Hello, I am a bot!\r\n";
-        send(fd, response.c_str(), response.size(), MSG_EOR);
-    }
-    else {
-        std::cout << "This is a channel message." << std::endl;
-        std::string response = "PRIVMSG " + msg.params[0] + " :Hello, I am a bot!\r\n";
-        send(fd, response.c_str(), response.size(), MSG_EOR);
-    }
-=======
 	if (msg.from == "Bot")
 		return ;
 
@@ -61,5 +45,4 @@ void Bot::CmPrivMsg(t_msg &msg)
 				}
 			}
 		}
->>>>>>> origin/bot
 }
